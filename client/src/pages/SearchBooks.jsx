fe51b9e4bb4268247b9634f020fc31bf38dd215d
yahm0 <stnt190@outlook.com>
2024-05-19 14:33:26 -0400

    added logic to the SearchBooks.jsx. The const SearchBooks and some funtionality.

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -3,3 +3,24 @@ import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
 import { searchGoogleBooks } from '../utils/API';
 import Auth from '../utils/auth';
+
+
+const SearchBooks = () => {
+    const [saveBook] = useMutation(SAVE_BOOK);
+    const [searchInput, setSearchInput] = useState('');
+    const [searchedBooks, setSearchedBooks] = useState([]);
+  
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+  
+      if (!searchInput) {
+        return false;
+      }
+  
+      try {
+        const response = await searchGoogleBooks(searchInput);
+  
+        if (!response.ok) {
+          throw new Error('something went wrong!');
+        }
+  
