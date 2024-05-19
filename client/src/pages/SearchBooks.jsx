caa83f819f5825837ee3cc92048179f1631365d7
yahm0 <stnt190@outlook.com>
2024-05-19 14:34:18 -0400

    added logic to the SearchBooks.jsx. The const bookData for the item map

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -24,3 +24,22 @@ const SearchBooks = () => {
           throw new Error('something went wrong!');
         }
   
+        const { items } = await response.json();
+
+      const bookData = items.map((book) => ({
+        bookId: book.id,
+        authors: book.volumeInfo.authors || ['No author to display'],
+        title: book.volumeInfo.title,
+        description: book.volumeInfo.description,
+        image: book.volumeInfo.imageLinks?.thumbnail || '',
+        link: book.volumeInfo.infoLink,
+      }));
+
+      setSearchedBooks(bookData);
+      setSearchInput('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  
