b18d1d3f0e1b3bff84a4b1cee1ec2deddf7d8f4b
yahm0 <stnt190@outlook.com>
2024-05-19 12:19:24 -0400

    add logic to server.js. to apply the middleware

--- server/server.js
+++ server/server.js
@@ -13,3 +13,12 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
