c3d6970fc94a33d35c96d67d9cff9c3386de8ff3
yahm0 <stnt190@outlook.com>
2024-05-19 12:25:03 -0400

    added logic to the resolvers.js file for mutations to remove books and to export the file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -48,3 +48,19 @@ const resolvers = {
             }
             throw new AuthenticationError('Not logged in');
           },
+
+          removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+              );
+              return updatedUser;
+            }
+            throw new AuthenticationError('Not logged in');
+          },
+        },
+      };
+      
+      module.exports = resolvers;
