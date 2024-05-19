68f4022eca8d3da7781974b9ca471f0644799fe1
yahm0 <stnt190@outlook.com>
2024-05-19 12:18:53 -0400

    add logic to server.js. to initalize it

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
