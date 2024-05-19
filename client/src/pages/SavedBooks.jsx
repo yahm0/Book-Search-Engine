bc56d364211248140eca1419fc6934ce561f533f
yahm0 <stnt190@outlook.com>
2024-05-19 14:37:22 -0400

    added logic to savedbooks and the API.js

---
+++ client/src/pages/SavedBooks.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const SavedBooks = () => {
+  const { loading, data } = useQuery(GET_ME);
+  const [removeBook] = useMutation(REMOVE_BOOK);
+
+  const userData = data?.me || {};
+
+  const handleDeleteBook = async (bookId) => {
+    try {
+      await removeBook({
+        variables: { bookId },
+      });
+
+      console.log('book removed');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
